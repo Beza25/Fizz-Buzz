a3687b5ba2f3602bcc5eb4655f813b0da84a49bb
Beza25 <sirak.bezawit@gmail.com>
2019-09-10 13:40:15 -0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,25 @@ public class Main {
     public static void main(String[] args){
 
         int message = 0;
-        String str;
+        String str, five;
 
-        for(int i =1; i <= 12; i++){
+        for(int i =1; i <= 100; i++){
             message = i;
             if(i%3 == 0){
                 str = Integer.toString(message);
                 str = "Fizz";
+                System.out.println(str);
 
 
             }else if(i%5 == 0){
-                str = Integer.toString(message);
-                str = "Buzz";
+                five = Integer.toString(message);
+                five = "Buzz";
+                System.out.println(five);
 
             }else{
-                continue;
+                System.out.println(i);
             }
-            System.out.println(message);
+
 
         }
 
